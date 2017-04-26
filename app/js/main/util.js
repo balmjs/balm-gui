cf7db52c6fc14aa2035548999651e948bf145d88
74sharlock <sharlock7788@gmail.com>
2017-04-26 11:37:24 +0800

    update util

--- app/js/main/util.js
+++ app/js/main/util.js
@@ -1,25 +1,7 @@
 const {exec} = require('child_process');
 const fs = require('fs');
 
-const type = function (everything) {
-  return ({}).toString.call(everything).replace(/\[object\s(.*)]/, '$1').toLowerCase();
-};
-
-[
-  'String',
-  'Object',
-  'Number',
-  'Boolean',
-  'Function',
-  'Array',
-  'Undefined',
-  'Null'
-].forEach(function (typeString) {
-  type[`is${typeString}`] = (n) => type(n) === typeString.toLowerCase();
-});
-
 const utils = {
-  type,
   execHandle (command, sender) {
     let ls = exec(command);
 
